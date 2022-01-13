037cb7dfccec6d7d83daf04c8ad81227246c89a5
Davy SEBBAH <79283100+DavyHaYehoudi@users.noreply.github.com>
2022-01-13 09:05:59 +0200

    selection afficahge

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {plantList} from '../datas/plantList';
 import Cards from './Cards';
 import '../styles/articles.css'
@@ -6,22 +6,40 @@ import '../styles/articles.css'
 
 const Articles = () => {
 
+    const[selected,setSelected]= useState('Toutes');
 
+    const handleSelected = (e) => {
+        setSelected(e.target.value)
+    }
+
+    const handleReset = (e) => {
+        setSelected('Toutes')
+    }
+
+    
     return (
         <div>
             <div className='blocSelectReset'>
-                <select name='select' id='select'>
-                    <option value='---'>---</option>
+                <select name='select' id='select' value={selected} onChange={handleSelected}>
+                    <option value='Toutes'>Toutes</option>
                     <option value='classique'>classique</option>
                     <option value='extérieur'>extérieur</option>
                     <option value='plante grasse'>plante grasse</option>
                 </select>
-                <input type='reset' />
+                <input type='reset' onClick={handleReset}/>
             </div>
             <div className='listeArticles'>
-                    {plantList.map(plant => 
+                {selected==='Toutes' ? 
+                (plantList
+                    .map(plant => 
+                        <Cards key={plant.id} plant={plant}/>))
+                        :
+                    (plantList
+                        .filter(element => element.category===selected)
+                        .map(plant => 
                         <Cards key={plant.id} plant={plant}/>
-                    )}
+                    ))
+                }
             </div>
         </div>
     );
